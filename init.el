fbc9f1b0918ffaa8b1051d3b670a780d2f86136f
Zakaria Kebairia <4.kebairia@gmail.com>
2021-01-27 11:23:37 +0100

    cleaning up, fixing some problems[mu4e,latex,tab indentation]

--- init.el
+++ init.el
@@ -42,17 +42,3 @@
                                 (number-to-string (emacs-pid))
                                 ") started in "
                                 (emacs-init-time)))))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(magit-display-buffer-function 'magit-display-buffer-traditional)
- '(package-selected-packages
-   '(org-mu4e zetteldeft yaml-mode which-key use-package undo-tree try sqlite3 smtpmail-multi sml-modeline smart-mode-line-powerline-theme rainbow-mode rainbow-delimiters projectile pbcopy ox-twbs ox-tufte ox-reveal ox-hugo outorg org-roam org-ref org-pdfview org-pdftools org-mime org-download org-cliplink ob-async notmuch mu4e-views mu4e-overview mpv minions memoize markdown-mode+ lsp-mode keyfreq jedi hl-todo helm-swoop helm-org gruvbox-theme graphviz-dot-mode fzf flycheck f3 evil-org evil-mu4e evil-magit evil-leader evil-collection eterm-256color ess emacsql-sqlite elpy elfeed-org dired-subtree dired-single counsel command-log-mode binder anki-editor anki-connect aggressive-indent)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
