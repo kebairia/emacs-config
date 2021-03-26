3ccc18768d88d03d8d835cf2b3b3e297bfd33d5e
Zakaria Kebairia <4.kebairia@gmail.com>
2021-03-26 14:23:26 +0100

    disable mu4e

--- modules/MU4E.el
+++ modules/MU4E.el
@@ -1,23 +1,24 @@
+(require 'org-mime)
 (use-package mu4e
   :ensure nil
   :config
-  ;; Pull in org helpers
-  (require 'mu4e-org)
   ;; This is set to 't' to avoid mail syncing issues when using mbsync
   (setq mu4e-change-filenames-when-moving t)
-                                        ; allow fancy icons for mail threads
-  ;;(setq mu4e-use-fancy-chars t) 
 
   ;; Refresh mail using isync every 10 minutes
   ;;(setq mu4e-update-interval (* 10 60))
-  (setq mu4e-get-mail-command "mbsync --config ~/.config/isync/mbsyncrc -a"
+  (setq mu4e-get-mail-command "mbsync -a"
+        ;; mu4e-html2text-command "w3m -T text/html" ;;using the default mu4e-shr2text
+        mu4e-view-prefer-html t
+        mu4e-update-interval 180
+        mu4e-headers-auto-update t
         mu4e-compose-signature-auto-include t
         mu4e-compose-signature 
         (concat "Kebairia Zakaria\n"
                 "Github: www.github.com/kebairia\n"
                 "linkedIn: www.linkedin.com/in/zakaria-kebairia\n")
-        mu4e-compose-format-flowed t) ;; Make sure plain text mails flow correctly for recipients
-
+        mu4e-compose-format-flowed t ;; Make sure plain text mails flow correctly for recipients
+        )
 
   (setq mu4e-maildir (expand-file-name "~/.local/share/mail"))
 
@@ -32,11 +33,10 @@
               (string-prefix-p "/ESI" (mu4e-message-field msg :maildir))))
           :vars '((user-mail-address . "z.kebairia@esi-sba.dz")
                   (user-full-name    . "Kebairia Zakaria, ISI, G03")
-                  (smtpmail-auth-credentials . (expand-file-name "~/.local/share/emacs/authinfo"))
-                  (smtpmail-smtp-user . "z.kebairia@esi-sba.dz")
                   (smtpmail-smtp-server  . "smtp.gmail.com")
-                  (smtpmail-smtp-service . 465)
+                  (smtpmail-smtp-service . 587)
                   (smtpmail-stream-type  . tls)
+                  (smtpmail-auth-credentials . (expand-file-name "~/.local/share/emacs/authinfo"))
                   (mu4e-drafts-folder  . "/z.kebairia@esi-sba.dz/[Gmail].Drafts")
                   (mu4e-sent-folder  . "/z.kebairia@esi-sba.dz/[Gmail].Sent Mail")
                   (mu4e-refile-folder  . "/z.kebairia@esi-sba.dz/[Gmail].All Mail")
@@ -52,59 +52,49 @@
               (string-prefix-p "/4.kebairia" (mu4e-message-field msg :maildir))))
           :vars '((user-mail-address . "4.kebairia@gmail.com")
                   (user-full-name    . "Kebairia Zakaria")
-                  (smtpmail-auth-credentials . (expand-file-name "~/.local/share/emacs/authinfo"))
-                  (smtpmail-smtp-user . "4.kebairia@gmail.com")
+                  (smtpmail-starttls-credentials . (("smtp.gmail.com" 587 nil nil)))
                   (smtpmail-smtp-server  . "smtp.gmail.com")
-                  (smtpmail-smtp-service . 465)
+                  (smtpmail-smtp-service . 587)
                   (smtpmail-stream-type  . tls)
+                  (smtpmail-auth-credentials . (expand-file-name "~/.local/share/emacs/authinfo"))
+                  (smtpmail-debug-info . t)
+                  (smtpmail-debug-verbose . t)
                   (mu4e-drafts-folder  . "/4.kebairia@gmail.com/[Gmail].Drafts")
                   (mu4e-sent-folder  . "/4.kebairia@gmail.com/[Gmail].Sent")
                   (mu4e-refile-folder  . "/4.kebairia@gmail.com/[Gmail].Archive")
                   (mu4e-trash-folder  . "/4.kebairia@gmail.com/[Gmail].Trash")
                   )))))
-
-;; use org-htmlize by default when sending an email
-(add-hook 'message-send-hook 'org-mime-htmlize)
-
 ;; Configure the function to use for sending mail
 (setq message-send-mail-function 'smtpmail-send-it)
-(use-package org-mime
-:ensure t
-:config
-(setq org-mime-export-options '(
-                                :section-numbers nil
-                                :with-author nil
-                                :with-toc nil)))
 
-;; (use-package org-mu4e
-;;   :ensure t)
 ;; spell check
 (add-hook 'mu4e-compose-mode-hook
           (defun my-do-compose-stuff ()
             "My settings for message composition."
             (visual-line-mode)
-            ;;(mu4e-org-mode)
+            (org-mu4e-compose-org-mode)
             (use-hard-newlines -1)
             (flyspell-mode)))
 
 (add-hook 'mu4e-headers-mode-hook
-(defun my/mu4e-change-headers ()
-  (interactive)
-  (setq mu4e-headers-fields
-        `((:human-date . 15) ;; alternatively, use :date
-          (:flags . 6)
-          (:from . 22)
-          (:thread-subject . ,(- (window-body-width) 70)) ;; alternatively, use :subject
-          (:size . 7)))))
-
+          (defun my/mu4e-change-headers ()
+            (interactive)
+            (setq mu4e-headers-fields
+                  `((:human-date . 10) ;; alternatively, use :date
+                    (:flags . 6)
+                    (:from . 22)
+                    (:thread-subject . ,(- (window-body-width) 70)) ;; alternatively, use :subject
+                    (:size . 7)))))
+(setq org-mu4e-convert-to-html t)
 ;;from the info manual
 (setq mu4e-attachment-dir  "~/dwn")
 
 (setq message-kill-buffer-on-exit t)
 (setq mu4e-compose-dont-reply-to-self t)
 
+(require 'org-mu4e)
+
 ;; convert org mode to HTML automatically
 (setq org-mu4e-convert-to-html t)
-
 ;; don't ask when quitting
 (setq mu4e-confirm-quit nil)
