9b3ef96f05edac610ad4f0bfb85819549bd50a64
Zakaria Kebairia <4.kebairia@gmail.com>
2021-06-12 15:39:52 +0100

    updating revealjs
    adding new revealjs version, use another emacs-revealjs framework

--- init.el
+++ init.el
@@ -31,10 +31,12 @@
 (load "~/.config/emacs/modules/BINDINGS.el") 
 (load "~/.config/emacs/modules/ORG.el") 
 (load "~/.config/emacs/modules/ORG-ROAM.el") 
-;(load "~/.config/emacs/modules/MU4E.el") 
+                                        ;(load "~/.config/emacs/modules/MU4E.el") 
 (load "~/.config/emacs/nano/nano.el") 	;
 (load "~/.config/emacs/nano/nano-layout.el") 
-(load "~/.config/emacs/nano/nano-modeline.el")
+(load "~/.config/emacs/nano/nano-modeline.el") 
+(add-to-list 'load-path "~/.config/emacs/emacs-reveal")
+(require 'emacs-reveal)
 
 ;; Measure emacs startup time
 (add-to-list 'after-init-hook
--- modules/ORG.el
+++ modules/ORG.el
@@ -165,7 +165,7 @@
 (use-package ox-reveal
   :ensure ox-reveal)
 (setq org-reveal-root
-      "file:///home/zakaria/org/conf/revealJS/reveal.js-4.1.0")
+      "file:///home/zakaria/org/conf/revealJS/reveal.js-4.1.2")
 (setq org-reveal-mathjax t)
 
 (eval-after-load "org"
