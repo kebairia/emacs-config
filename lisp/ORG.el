0036957e4b6d8d4380e1c00c9dbe7f93f65f2e8d
Zakaria Kebairia <4.kebairia@gmail.com>
2021-07-22 19:45:10 +0100

    cleaning

--- lisp/ORG.el
+++ lisp/ORG.el
@@ -254,7 +254,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(magit-display-buffer-function 'magit-display-buffer-traditional)
  '(org-export-backends '(ascii beamer html icalendar latex odt)))
 
 (custom-set-variables
--- lisp/PYTHON.el
+++
@@ -1,2 +0,0 @@
-(use-package flycheck
-:init (global-flycheck-mode))
