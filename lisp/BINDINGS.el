376a5b633eb00d66c0a6de933a2cf2a4ab6a8012
Zakaria Kebairia <4.kebairia@gmail.com>
2021-09-03 11:20:55 +0100

    adding keybinding to my global bibliography

--- lisp/BINDINGS.el
+++ lisp/BINDINGS.el
@@ -3,10 +3,11 @@
 ;; (set-register ?b (cons 'file "~/org/books.org" ))
 (global-set-key (kbd "C-c C") (lambda() (interactive)(find-file "~/.config/emacs/init.org")))
 (global-set-key (kbd "C-c b") (lambda() (interactive)(find-file "~/org/books.org")))
+(global-set-key (kbd "C-c r") (lambda() (interactive)(find-file "~/org/refs.org")))
 (global-set-key (kbd "C-c I") (lambda() (interactive)(find-file "~/org/gtd/inbox.org")))
 (global-set-key (kbd "C-c L") (lambda() (interactive)(find-file "~/org/links.org")))
-(global-set-key (kbd "C-c E") (lambda() (interactive)(find-file "~/org/gtd/emails.org")))
 (global-set-key (kbd "<f12>") (lambda() (interactive)(find-file "~/org/conf/org.pdf")))
+;; (global-set-key (kbd "C-c E") (lambda() (interactive)(find-file "~/org/gtd/emails.org")))
 ;; Reload buffer with <F5>
 (global-set-key [f5] '(lambda () (interactive) (revert-buffer nil t nil)))
 
