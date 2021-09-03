5aebd9e573f9c458423ec254da8fb4605124a3b6
Zakaria Kebairia <4.kebairia@gmail.com>
2021-09-03 11:22:12 +0100

    adding org-roam-ui config

--- lisp/ORG-ROAM.el
+++ lisp/ORG-ROAM.el
@@ -40,6 +40,37 @@
 (define-key winner-mode-map (kbd "<M-left>") #'winner-undo)
 (define-key winner-mode-map (kbd "<M-right>") #'winner-redo)
 
+;;   )
+
+(use-package org-roam-ui
+  :straight
+  (:host github :repo "org-roam/org-roam-ui" :branch "main" :files ("*.el" "out"))
+  :after org-roam
+  ;; :hook
+  ;;         normally we'd recommend hooking orui after org-roam, but since org-roam does not have
+  ;;         a hookable mode anymore, you're advised to pick something yourself
+  ;;         if you don't care about startup time, use
+  ;;  :hook (after-init . org-roam-ui-mode)
+  :config
+  (setq org-roam-ui-sync-theme nil
+        org-roam-ui-follow t
+        org-roam-ui-update-on-save t
+        org-roam-ui-open-on-start t))
+
+;; (setq org-roam-ui-custom-theme
+;;       '((bg . "#1d2021")
+;;         (bg-alt . "#282a36")
+;;         (fg . "#f8f8f2")
+;;         (fg-alt . "#6272a4")
+;;         (red . "#ff5555")
+;;         (orange . "#f1fa8c")
+;;         (yellow ."#ffb86c")
+;;         (green . "#50fa7b")
+;;         (cyan . "#8be9fd")
+;;         (blue . "#ff79c6")
+;;         (violet . "#8be9fd")
+;;         (magenta . "#bd93f9")))
+
 (setq org-roam-graph-viewer
       (lambda (file)
         (let ((org-roam-graph-viewer "/usr/bin/brave"))
