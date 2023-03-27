74726f986f126ffcb0a9cf02917996b736b25b03
Zakaria Kebairia <4.kebairia@gmail.com>
2023-03-27 15:52:34 +0100

    Adding initial code

--- init.el
+++ init.el
@@ -1,8 +1,139 @@
+;; -*- lexical-binding: t -*-
+;; This file has been generated from init.org file. DO NOT EDIT.
+;; Sources are available from https://github.com/kebairia/emacs-config
+
+;; Copyright (C) 2023 Kebairia Zakaria
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; For a full copy of the GNU General Public License
+;; see <https://www.gnu.org/licenses/>.
+
+(defvar my/init-start-time (current-time) "Time when init.el was started")
+(defvar my/section-start-time (current-time) "Time when section was started")
+(defun my/report-time (section)
+  (message "%-36s %.2fs"
+           (concat section " " "section time: ")
+           (float-time (time-subtract (current-time) my/section-start-time))))
+(message "---------------------------------------------------------------")
+
+(setq straight-check-for-modifications nil)
+
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(add-to-list 'load-path
+             (expand-file-name "lisp" user-emacs-directory))
+(add-to-list 'custom-theme-load-path
+             (expand-file-name "theme" user-emacs-directory))
+
+(setq package-list
+      '(evil                ; A VI layer for Emacs
+        evil-collection
+        evil-org
+        f                   ; Modern API for working with files and directories
+        citar               ; Citation-related commands for org, latex, markdown
+        citeproc            ; A CSL 1.0.2 Citation Processor
+        flyspell-correct-popup ; Correcting words with flyspell via popup interface
+        flyspell-popup      ; Correcting words with Flyspell in popup menus
+        helpful             ; A better help buffer
+        htmlize             ; Convert buffer text and decorations to HTML
+        imenu-list          ; Show imenu entries in a separate buffer
+        magit               ; A Git porcelain inside Emacs.
+        markdown-mode       ; Major mode for Markdown-formatted text
+        use-package         ; A configuration macro for simplifying your .emacs
+        yaml-mode           ; YAML mode
+        ;; org-auto-tangle     ; Tangle org file when it is saved
+        which-key))         ; Display available keybindings in popup
+
+;; Install packages that are not yet installed
+(dolist (package package-list)
+  (straight-use-package package))
+
+;; Special case for pdf-tools that has recently (2022) changed maintainer
+(straight-use-package
+ '(pdf-tools :type git :host github :repo "vedang/pdf-tools"))
+
+;; ;; Denote not yet on ELPA (2022-06-19)
+;; (straight-use-package
+;;  '(denote :type git :host github :repo "protesilaos/denote"))
+
+;; Display org properties in the agenda buffer (modified version)
+;; (straight-use-package
+;;  '(org-agenda-property :type git :host github :repo "Malabarba/org-agenda-property"
+;;                        :fork (:host github :repo "rougier/org-agenda-property")))
+
+;; NANO splash
+;; (straight-use-package
+;;  '(nano-splash :type git :host github :repo "rougier/nano-splash"))
+
+;; NANO theme
+(straight-use-package
+ '(nano-theme :type git :host github :repo "rougier/nano-theme"))
+
+;; NANO modeline
+(straight-use-package
+ '(nano-modeline :type git :host github :repo "rougier/nano-modeline"))
+
+;; NANO agenda
+(straight-use-package
+ '(nano-agenda :type git :host github :repo "rougier/nano-agenda"))
+
+;; NANO agenda
+(straight-use-package
+ '(minibuffer-header :type git :host github :repo "rougier/minibuffer-header"))
+
+;; SVG tags, progress bars & icons
+(straight-use-package
+ '(svg-lib :type git :host github :repo "rougier/svg-lib"))
+
+;; Replace keywords with SVG tags
+(straight-use-package
+ '(svg-tag-mode :type git :host github :repo "rougier/svg-tag-mode"))
+
+;; ;; Relative date formatting
+;; (straight-use-package
+;;  '(relative-date :type git :host github :repo "rougier/relative-date"))
+
+;; org imenu
+;; (straight-use-package
+;;  '(org-imenu :type git :host github :repo "rougier/org-imenu"))
+
+;; Bilbliography manager in org mode
+(straight-use-package
+  '(org-bib :type git :host github :branch "org-imenu" :repo "rougier/org-bib-mode"))
+
 (setq-default
- ad-redefinition-action 'accept         ; Silence warnings for redefinition
- custom-unlispify-menu-entries nil      ; Prefer kebab-case for titles
- custom-unlispify-tag-names nil         ; Prefer kebab-case for symbols
-(put 'downcase-region 'disabled nil)    ; Enable downcase-region
-(put 'upcase-region 'disabled nil)      ; Enable upcase-region
- native-comp-async-report-warnings-errors 'silent ; Skip compilation error buffers
- read-process-output-max (* 1024 1024)  ; Increase read size per process
+ inhibit-startup-screen t               ; Disable start-up screen
+ inhibit-startup-message t              ; Disable startup message
+ inhibit-startup-echo-area-message t    ; Disable initial echo message
+ initial-scratch-message ""             ; Empty the initial *scratch* buffer
+ initial-buffer-choice t)               ; Open *scratch* buffer at init
+
+(require 'nano-theme)
+(setq nano-fonts-use t) ; Use theme font stack
+(nano-dark)            ; Use theme light version
+(nano-mode)  
+(require 'nano-theme)
+(require 'nano-modeline)
+
+(setq nano-modeline-prefix 'status)
+(setq nano-modeline-prefix-padding 1)
